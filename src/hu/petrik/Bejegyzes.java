e7bf4073e10e933e644040fca1624b6cf8c1eba8
Domokos Dávid <you@example.com>
2021-09-29 15:02:13 +0200

    Osztály példányosítása

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -1,5 +1,48 @@
 package hu.petrik;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Bejegyzes {
-    
+    private String szerzo;
+    private  String tartalom;
+    private int likeok;
+    private LocalDateTime letrejott;
+    private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom){
+        likeok = 0;
+        letrejott = LocalDateTime.now();
+        szerkesztve = LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return szerkesztve;
+    }
+    public void like(){
+        likeok++;
+    }
 }
+
+
