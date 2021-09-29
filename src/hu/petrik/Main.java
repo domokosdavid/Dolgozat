42584fa107df417eff6290cc22c68dffe89b2382
Domokos Dávid <you@example.com>
2021-09-29 16:33:50 +0200

    A felhasználó módosítsa a 2. bejegyzést

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -12,18 +12,16 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> lista= new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
-
-        ArrayList<String> lista= new ArrayList<>();
 
 
         System.out.print("Kérek egy darabszámot!");
         int darabszam = sc.nextInt();
         String bejegyzes;
         int szamlalo = 0;
-
 
             do {
                 szamlalo++;
@@ -35,31 +33,41 @@ public class Main {
             while (szamlalo!=darabszam);
 
 
-                BufferedReader objReader = null;
-                try {
-                    String strCurrentLine;
-
-                    objReader = new BufferedReader(new FileReader("bejegyzesek.txt"));
-
-                    while ((strCurrentLine = objReader.readLine()) != null) {
-                        lista.add(strCurrentLine);
-                    }
-
-                } catch (IOException e) {
-
-                    e.printStackTrace();
-
-                } finally {
-
-                    try {
-                        if (objReader != null)
-                            objReader.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
+            Beolvas();
+            masodikBejegyzesModositasa();
 
 
 
+
+    }
+    public static void Beolvas(){
+        BufferedReader objReader = null;
+        try {
+            String strCurrentLine;
+
+            objReader = new BufferedReader(new FileReader("bejegyzesek.txt"));
+
+            while ((strCurrentLine = objReader.readLine()) != null) {
+                lista.add(strCurrentLine);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+                if (objReader != null)
+                    objReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static void masodikBejegyzesModositasa(){
+        System.out.println("Módosítsa a második bejegyzést!");
+        String ujbejegyzes;
+        lista.set(1,ujbejegyzes = sc.next());
     }
 }
