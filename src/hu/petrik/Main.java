60aaab2f3f8814a87490fca9299ff5c0fc43d94c
Domokos Dávid <you@example.com>
2021-09-29 15:48:32 +0200

    Darabszám, bejegyzés bekérése a felhasználótól, listához adás

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,16 +1,34 @@
 package hu.petrik;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
 
-    private ArrayList<Object> bejegyzesLista;
+    public static void main(String[] args) {
 
-    public void main(String[] args) {
 
-        this.bejegyzesLista = new ArrayList<>();
-        Bejegyzes[] bejegyzesTomb = new Bejegyzes[2];
+        ArrayList<String> lista= new ArrayList<>();
 
+        System.out.print("Kérek egy darabszámot!");
+        int darabszam = sc.nextInt();
+        String bejegyzes;
+        int szamlalo = 0;
+            do {
+                szamlalo++;
+                System.out.println("Bejegyzés:");
+                bejegyzes = sc.next();
+                lista.add(bejegyzes);
+
+            }
+            while (szamlalo!=darabszam);
+
+        for (String item:lista
+             ) {
+            System.out.println(item);
+        }
 
     }
 }
