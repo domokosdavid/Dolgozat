fddb7884ace11baea55502e524571a386e4ac011
Domokos Dávid <you@example.com>
2021-09-29 16:19:55 +0200

    Fájl beolvasása, hozzáadása listához (nem bontottam szerzőre,tartalomra)

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -1,21 +1,30 @@
 package hu.petrik;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         ArrayList<String> lista= new ArrayList<>();
+
 
         System.out.print("Kérek egy darabszámot!");
         int darabszam = sc.nextInt();
         String bejegyzes;
         int szamlalo = 0;
+
+
             do {
                 szamlalo++;
                 System.out.println("Bejegyzés:");
@@ -25,10 +34,32 @@ public class Main {
             }
             while (szamlalo!=darabszam);
 
-        for (String item:lista
-             ) {
-            System.out.println(item);
-        }
+
+                BufferedReader objReader = null;
+                try {
+                    String strCurrentLine;
+
+                    objReader = new BufferedReader(new FileReader("bejegyzesek.txt"));
+
+                    while ((strCurrentLine = objReader.readLine()) != null) {
+                        lista.add(strCurrentLine);
+                    }
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                } finally {
+
+                    try {
+                        if (objReader != null)
+                            objReader.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+
 
     }
 }
